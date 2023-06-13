3c133df3bb56009033de0f60393fd9322b08eac2
Dollars Kenthouky <105455669+Kenthouky@users.noreply.github.com>
2023-06-13 07:45:30 +0100

    change

--- 0x08-making_change /0-making_change.py
+++ 0x08-making_change /0-making_change.py
@@ -15,17 +15,18 @@ def makeChange(coins, total):
     determine the fewest number of coins needed to meet a given amount total
 
     """
-
+    
     if total <= 0:
-
         return 0
 
-    dp = [0] + [float("inf")] * (total)
-
-    for coin in coins:
-
-        for i in range(coin, total + 1):
-
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return dp[-1] if dp[-1] != float("inf") else -1
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
